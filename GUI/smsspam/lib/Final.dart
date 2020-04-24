60f19d0f1a38c0745f5b217833ce3a920b3db11a
gadarajainik <57976403+gadarajainik@users.noreply.github.com>
2020-04-24 18:16:45 +0530

    24/4/2020commit

--- GUI/smsspam/lib/Final.dart
+++ GUI/smsspam/lib/Final.dart
@@ -22,44 +22,29 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class Details {
-  List<SmsMessage> messages;
-  SmsQuery query;
-  Future<List<SmsMessage>> fun() async {
-    query = new SmsQuery();
-    messages = await query.getAllSms;
-    return messages;
-  }
-}
 
 class _MyHomePageState extends State<MyHomePage> {
   List<SmsMessage> messages;
   SmsQuery query;
   String res;
+  
+  //Read messages from INBOX
   Future<void> fun() async {
     query = new SmsQuery();
     messages = await query.getAllSms;
   }
 
-  String check(String s) {
+  
+  //Predict SPAM or HAM
+  String predict(String s) {
     String res;
-    http.get('https://585a1ec7.ngrok.io/?msg=User 25384: Your Gmail profile has been compromised. Text back SENDNOW in order to reactivate your account.').then((x) {
-      print(x.body);
+    http.get('https://585a1ec7.ngrok.io/?msg='+s).then((x) {
       res = x.body;
     });
     return res;
@@ -67,18 +52,13 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    this.fun();
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+    
+	this.fun();
+    
+	return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        
+		title: Text(widget.title),
         actions: <Widget>[new Icon(Icons.more_vert)],
       ),
       body: new ListView.builder(
@@ -112,22 +92,24 @@ class _MyHomePageState extends State<MyHomePage> {
                     check(messages[i].body).toString(),
                     maxLines: 1,
                   )),
+			  trailing:Text(check(messages[index].body)),
             )
           ],
         ),
-        //     children: new List.generate(messages.length, (int index) {
-        //   return new ListTile(
-
-        //     leading: Icon(Icons.account_circle),
-        //     title: Text(messages[index].sender),
-        //     subtitle: new Text(
-        //       messages[index].body,
-        //       maxLines: 1,
-        //     ),
-        //     // title: Text(check(messages[index].body)as String),
-        //   );
-        // })
       ),
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
